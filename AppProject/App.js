30c0a0725d8149e69521b1d0caf7e6641dcf4093
Jess-Halley <jmmhalley@gmail.com>
2017-07-11 16:14:22 +1200

    setting up dev enviroment for react native

---
+++ AppProject/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>RecycleHack</Text>
+        <Text>What would you like to recycle?</Text>
+        <Text>Explore Sustainability</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
